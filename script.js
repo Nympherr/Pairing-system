fb0a003420efccfac7870e87c91819ab30ca211c
Lukas St <114870613+Nympherr@users.noreply.github.com>
2023-05-12 18:00:44 +0300

    Add range slider, pairing box title and content
    Whenever "generate" button is clicked the pairing box will fill up
    with pairings depending on how many players were selected (not real
    pairings for now). Add slider beneath pairing box and title. Slider
    will change the output of the round number in the pairing box title.

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 let button = document.getElementById("generate-pairings");
 let startMenu = document.querySelector("body");
-let test = document.getElementById("test");
+let pairingDiv = document.getElementById("pairing-div");
+let pairingBox = document.getElementById("pairing-box");
 let body = document.querySelector("body");
+let playerNumber = document.getElementById("player-select");
+let slider = document.getElementById("roundSlider");
+let pairingTitle = document.getElementById("pairing-title");
+
+slider.addEventListener("input", function(){
+        pairingTitle.textContent = "Round " + slider.value ;
+});
 
 button.addEventListener("click", function(){
 
-    test.style.display = "block";
+    pairingDiv.style.display = "block";
+    pairingDiv.style.display = "flex";
     body.style.gap = "50px";
+
+    slider.min = 1;
+    slider.max = playerNumber.value - 1;
+    slider.value = 1;
+
+    fillPairingBox(playerNumber.value);
+    console.log(playerNumber.value);
+
 });
+
+function fillPairingBox(numberOfPlayers){
+
+    pairingBox.innerHTML = "";
+
+    pairingBox.style.display = "flex";
+    pairingBox.style.flexDirection = "column";
+    pairingBox.style.justifyContent = "space-evenly";
+    for(let i = 1; i <= numberOfPlayers; i++){
+
+        let div = document.createElement("div");
+        div.textContent = "Player vs Player " + i;
+        pairingBox.appendChild(div);
+    
+    }
+};
