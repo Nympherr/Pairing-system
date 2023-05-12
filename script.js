f982351077eb4d233c99d565e621151bf78b9cdc
Lukas St <114870613+Nympherr@users.noreply.github.com>
2023-05-12 23:03:40 +0300

    Add basic round robin logic
    Depending on amount of players, pairings for each round
    will be created in console for every player.Currently
    it does not take into account the color factor.

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ let playerNumber = document.getElementById("player-select");
 let slider = document.getElementById("roundSlider");
 let pairingTitle = document.getElementById("pairing-title");
 
+let playerArray = [];
+let scheduleArray = [];
+let numberOfRounds = 0;
+
+
 slider.addEventListener("input", function(){
         pairingTitle.textContent = "Round " + slider.value ;
 });
@@ -22,7 +27,11 @@ button.addEventListener("click", function(){
     slider.value = 1;
 
     fillPairingBox(playerNumber.value);
-    console.log(playerNumber.value);
+    createPlayer(playerNumber.value);
+    console.log(playerArray);
+    numberOfRounds = playerNumber.value - 1;
+
+    scheduleMaker(numberOfRounds, playerArray);
 
 });
 
@@ -41,3 +50,87 @@ function fillPairingBox(numberOfPlayers){
     
     }
 };
+
+function Person(name,whitePieces,blackPieces,rounds,opponents){
+    this.name = name;
+    this.whitePieces = whitePieces;
+    this.blackPieces = blackPieces;
+    this.rounds = rounds;
+    this.opponents = opponents;
+};
+
+function createPlayer(playerNumber){
+
+    if(playerNumber % 2 == 0){
+
+        for(let i = 1; i <= playerNumber; i++){
+
+            let player = new Person("Player " + i,0,0,0,[]);
+            playerArray.push(player);
+        };
+    }
+
+    else{
+
+        for(let i = 1; i <= playerNumber; i++){
+
+            let player = new Person("Player " + i,0,0,0,[]);
+            playerArray.push(player);
+        };
+        playerArray.push("BYE");
+    }
+};
+
+function scheduleMaker(numberOfRounds, playerArray){
+
+    if(numberOfRounds % 2 != 0){
+        for(let i = 1; i <= numberOfRounds; i++){
+
+            let roundPairings = [];
+
+            for (let j = 1; j <= (numberOfRounds + 1)/2; j++){
+
+                let pair = [];
+                pair.push(playerArray[j - 1]);
+                pair.push(playerArray[numberOfRounds - j + 1]);
+                roundPairings.push(pair);
+            }
+
+            scheduleArray.push(roundPairings);
+            shiftArray(playerArray);
+        };
+    }
+
+    else{
+
+        for(let i = 1; i <= numberOfRounds + 1; i++){
+
+            let roundPairings = [];
+
+            for (let j = 1; j <= (numberOfRounds + 2)/2; j++){
+
+                let pair = [];
+                pair.push(playerArray[j - 1]);
+                pair.push(playerArray[numberOfRounds - j + 2]);
+                roundPairings.push(pair);
+            }
+
+            scheduleArray.push(roundPairings);
+            shiftArray(playerArray);
+        };
+
+    }
+
+    console.log(scheduleArray);
+};
+
+function shiftArray(arr) {
+    const length = arr.length;
+    const lastElement = arr[length - 1];
+  
+    for (let i = length - 1; i > 0; i--) {
+      arr[i] = arr[i - 1];
+    }
+  
+    arr[1] = lastElement;
+  }
