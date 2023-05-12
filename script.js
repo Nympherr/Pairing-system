eb10cc3d525af43e14412243a9e0772bb10b0c6b
Lukas St <114870613+Nympherr@users.noreply.github.com>
2023-05-12 17:06:32 +0300

    "generate" button creates a box
    When clicked on "generate" button new box alongside start-menu
    will be created where pairings will be written in future versions.

---
+++ script.js
@@ -0,0 +1,10 @@
+let button = document.getElementById("generate-pairings");
+let startMenu = document.querySelector("body");
+let test = document.getElementById("test");
+let body = document.querySelector("body");
+
+button.addEventListener("click", function(){
+
+    test.style.display = "block";
+    body.style.gap = "50px";
+});
